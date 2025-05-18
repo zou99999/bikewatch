b5881b1ad129498b783ee80cfd7967aa0852d73e
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-05-17 20:09:55 -0700

    import mapbox

---
+++ map.js
@@ -0,0 +1,4 @@
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
