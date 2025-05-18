a7dfa6fdaed65f4ded8f886257597d7cc0996909
zou99999 <163795458+zou99999@users.noreply.github.com>
2025-05-17 20:29:17 -0700

    draw bike path

--- map.js
+++ map.js
@@ -1,8 +1,5 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-// Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
-
 // Set your Mapbox access token here
 mapboxgl.accessToken = 'pk.eyJ1IjoiZnpvdSIsImEiOiJjbWF0MnFlZnkwYTFqMnFvaXY5YmozdjNvIn0.W4o_J65TD__zq1HP9P6yhA';
 
@@ -15,3 +12,22 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+    //code
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+      });
+
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+          'line-color': 'green',
+          'line-width': 3,
+          'line-opacity': 0.4,
+        },
+      });
+  });
